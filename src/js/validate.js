e3793b64a9af2ebe18a50184ad36e32f704bce67
gudbrant <gudbrant@gmail.com>
2021-03-07 09:21:25 +0800

    0.0.4

---
+++ src/js/validate.js
@@ -0,0 +1,21 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-plusplus */
+
+export default function validate(value) {
+  if (value.length < 13) return false;
+  let sum = 0;
+  for (let i = 0; i < value.length; i++) {
+    let cardNum = parseInt(value[i], 10);
+
+    if ((value.length - i) % 2 === 0) {
+      cardNum *= 2;
+      if (cardNum > 9) {
+        cardNum -= 9;
+      }
+    }
+
+    sum += cardNum;
+  }
+
+  return (sum !== 0) && (sum % 10 === 0);
+}
